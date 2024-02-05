423355d9b3e7692ead3f034d9dba53d28b4f3d42
Eric Tsai <eric492718@gmail.com>
2024-02-05 15:46:49 +0800

    remove homepage endpoint

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Running");
